e95dd8d4e6b079c451cf7f4a1f46f90a9ba0635b
HardikSorathiya-CKF <hardik.sorathiya@cloudkodeform.com>
2025-07-07 21:17:00 +0530

    live through github

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,16 @@ const Header = ({ isOpen, toggle }) => {
   return (
     <nav className="navbar navbar-light bg-light shadow-sm px-3 d-flex justify-content-between align-items-center">
       {/* Left: Toggle Button */}
-      <button className="btn btn-primary" onClick={toggle}>
+      <button className="btn toggle-btn" onClick={toggle}>
         <i className={`bi ${isOpen ? 'bi-chevron-left' : 'bi-chevron-right'}`}></i>
       </button>
 
       {/* Right: Notification + Profile */}
       <div className="d-flex align-items-center gap-3">
         {/* Notification Icon */}
-        <button className="btn btn-light position-relative">
+        <button className="bel-sorath btn position-relative">
           <i className="bi bi-bell fs-5"></i>
-          <span className="position-absolute top-0 start-100 translate-middle p-1 bg-danger border border-light rounded-circle">
+          <span className="position-absolute translate-middle p-1 rounded-circle">
             <span className="visually-hidden">New alerts</span>
           </span>
         </button>
